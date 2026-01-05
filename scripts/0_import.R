902d19be53439ee8ca2a9703ffbffbf0f184da3d
elisehodbert <elisehodbert21@gmail.com>
2026-01-05 16:09:34 +0100

    Update 0_import.R

--- scripts/0_import.R
+++ scripts/0_import.R
@@ -3,6 +3,7 @@ pacman::p_load(
   openxlsx, tidyverse, ggpubr, colorspace, scales
 )
 
+# Creation of df_articles (one line per article)
 df_articles <- read.xlsx("grille_extract_test.xlsx") %>%
   filter(!str_detect(author_year, "_sub_")) %>%
   mutate(
@@ -36,43 +37,8 @@ df_articles <- read.xlsx("grille_extract_test.xlsx") %>%
     nb_transfers_year = nb_transfers / time_range_database_years
   )
 
-# 
-# # 1. Fonctions de recodage
-# recode_pathogen <- function(x) {
-#   recode(
-#     x,
-#     "MRSA"                                       = "S. aureus",
-#     "ESBL K. pneumoniae"                         = "K. pneumoniae",
-#     "Carbapenem-Resistant Enterobacterales"      = "Enterobacterales",
-#     "MDR Enterobacterales"                       = "Enterobacterales",
-#     "Vancomycin-Resistant Enterococci"           = "Enterococci",
-#     "Multiple (CRE, MRSA, MDR P. aeruginosa, C. difficile)" = "Multiple",
-#     .default = x
-#   )
-# }
-# 
-# # 2. Lecture et pré‐traitement des articles
-# df_articles <- read.xlsx(
-#   "grille_extract_test.xlsx"
-# ) %>%
-#   filter(!str_detect(author_year, "_sub_")) %>%
-#   mutate(
-#     transfer_type        = factor(transfer_type, levels = c("Direct", "Indirect")),
-#     across(c(nb_transfers, nb_facilities, nb_units, time_range_database_years), as.numeric),
-#     prop_model_pathogen  = recode_pathogen(prop_model_pathogen)
-#   ) %>%
-#   mutate(
-#     static_or_temporal = case_when(
-#       if_temporal_tempscale == "Yearly" ~ "Static",
-#       TRUE                              ~ as.character(static_or_temporal)),
-#       if_static_agr_or_avg = if_else(if_temporal_tempscale == "Yearly", "Aggregated", if_static_agr_or_avg),
-#     if_temporal_tempscale = if_else(if_temporal_tempscale == "Yearly", NA_character_, if_temporal_tempscale)
-#   ) %>%
-#   mutate(static_or_temporal = factor(static_or_temporal, levels = c("Static", "Temporal"))) %>%
-#   mutate(nb_transfers_year = nb_transfers / time_range_database_years)
 
-
-# 3. Lecture et pré‐traitement des réseaux
+# Creation of df_networks (one line per network presented)
 df_networks <- read.xlsx(
   "grille_extract_test.xlsx") %>%
   filter(network != "Not a network") %>%
@@ -84,8 +50,7 @@ df_networks <- read.xlsx(
     start_year_database = as.numeric(start_year_database)
   )
 
-# 4. Définition des formes et palettes
-
+# Definition of parameters: colors and shapes
 color_national = "#A8C5DA"
 color_subnational = "#B8D5B8"
 color_hospital = "#F47C57"
@@ -102,47 +67,6 @@ continent_palettes <- c(
   Oceania         = "Pastel 1"
 )
 
-# Fonction pour générer les couleurs
-# generate_distinct_colors <- function(n = 25, min_dist = 30, seed = 456) {
-#   if (!is.null(seed)) set.seed(seed)
-#   h_vals <- c(); c_vals <- c(); l_vals <- c()
-#   cols    <- character(0)
-#   
-#   while (length(cols) < n) {
-#     h0 <- runif(1, 0, 360)
-#     c0 <- runif(1, 20, 80)
-#     l0 <- runif(1, 30, 90)
-#     
-#     if (length(cols) > 0) {
-#       dh <- abs(h0 - h_vals)
-#       dh <- pmin(dh, 360 - dh)  
-#       dc <- c0 - c_vals
-#       dl <- l0 - l_vals
-#       dists <- sqrt(dh^2 + dc^2 + dl^2)
-#       if (min(dists) < min_dist) next
-#     }
-#     h_vals <- c(h_vals, h0)
-#     c_vals <- c(c_vals, c0)
-#     l_vals <- c(l_vals, l0)
-#     cols    <- c(cols, hcl(h = h0, c = c0, l = l0, fixup = TRUE))
-#   }
-#   cols
-# }
-
-# Exemple et visualisation
-# palette <- generate_distinct_colors(n = 25, min_dist = 40, seed = 123)
-# barplot(
-#   rep(1, length(palette)),
-#   col    = palette,
-#   border = NA,
-#   space  = 0,
-#   axes   = FALSE,
-#   main   = "25 couleurs éloignées les unes des autres"
-# )
-
-
-
-# 5. Construction de network_info avec couleurs et formes
 # 5. Construction de network_info avec couleurs et formes
 
 v1 = c("black","grey50", "grey80",
